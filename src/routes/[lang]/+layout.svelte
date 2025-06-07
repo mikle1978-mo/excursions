75f83267476ae0f47bfe59bcc3de90a87cfd46a8
mikle1978-mo <azimuth-m@yandex.ru>
2025-06-07 22:56:21 +0300

    250607 meta yandex google

--- src/routes/[lang]/+layout.svelte
+++ src/routes/[lang]/+layout.svelte
@@ -95,7 +95,7 @@
         name="twitter:image"
         content={`${baseUrl}/images/excursions/excursion_default.webp`}
     />
-
+    <meta name="yandex-verification" content="62268aabdebfd6ad" />
     <meta name="yandex-verification" content="2d9d922adb1a6ebf" />
 
     <!-- Google tag (gtag.js) -->
