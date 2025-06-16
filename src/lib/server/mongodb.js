cf8901a168f841ec34755602f65b837ceab0b40b
mikle1978-mo <azimuth-m@yandex.ru>
2025-06-16 15:13:58 +0300

    250616 seo

--- src/lib/server/mongodb.js
+++ src/lib/server/mongodb.js
@@ -1,21 +1,46 @@
-// src/lib/server/mongodb.js
 import { MongoClient } from "mongodb";
 import { env } from "$env/dynamic/private";
 
+if (!env.MONGODB_URI) {
+    throw new Error("MONGODB_URI не задан в переменных окружения");
+}
+
 let client;
+let clientPromise;
 let db;
+
+if (process.env.NODE_ENV === "development") {
+    // @ts-ignore
+    if (!global._mongoClientPromise) {
+        console.log("Создаём новый MongoClient и промис подключения (dev)");
+        client = new MongoClient(env.MONGODB_URI);
+        // @ts-ignore
+        global._mongoClientPromise = client.connect();
+    } else {
+        // @ts-ignore
+        console.log("Используем кешированный промис подключения (dev)");
+    }
+    // @ts-ignore
+    clientPromise = global._mongoClientPromise;
+} else {
+    console.log("Создаём новый MongoClient и промис подключения (prod)");
+    client = new MongoClient(env.MONGODB_URI);
+    clientPromise = client.connect();
+}
 
 export async function connectToDatabase() {
     try {
-        if (!client) {
-            client = new MongoClient(env.MONGODB_URI);
-            await client.connect();
-            console.log("✅ Успешное подключение к MongoDB");
-            db = client.db();
+        if (!db) {
+            console.log("Ожидаем подключения к MongoDB...");
+            const connectedClient = await clientPromise;
+            console.log("Подключение успешно");
+            db = connectedClient.db(); // Если нужно, здесь можно указать имя БД
+        } else {
+            console.log("Используем уже существующее подключение к базе");
         }
         return db;
-    } catch (e) {
-        console.error("❌ Ошибка при подключении к MongoDB", e);
-        throw e;
+    } catch (error) {
+        console.error("Ошибка при подключении к MongoDB:", error);
+        throw error;
     }
 }
