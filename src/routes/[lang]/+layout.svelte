86b00d45d5f3d1efcf0ddf36f9906c10023b4b7c
mikle1978-mo <azimuth-m@yandex.ru>
2025-06-22 14:47:14 +0300

    250622 metrica

--- src/routes/[lang]/+layout.svelte
+++ src/routes/[lang]/+layout.svelte
@@ -102,6 +102,51 @@
     <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
     <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
     <meta name="theme-color" content="#ffffff" />
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (m, e, t, r, i, k, a) {
+            m[i] =
+                m[i] ||
+                function () {
+                    (m[i].a = m[i].a || []).push(arguments);
+                };
+            m[i].l = 1 * new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                    return;
+                }
+            }
+            (k = e.createElement(t)),
+                (a = e.getElementsByTagName(t)[0]),
+                (k.async = 1),
+                (k.src = r),
+                a.parentNode.insertBefore(k, a);
+        })(
+            window,
+            document,
+            "script",
+            "https://mc.yandex.ru/metrika/tag.js",
+            "ym"
+        );
+
+        ym(97637253, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true,
+        });
+    </script>
+    <noscript
+        ><div>
+            <img
+                src="https://mc.yandex.ru/watch/97637253"
+                style="position:absolute; left:-9999px;"
+                alt=""
+            />
+        </div></noscript
+    >
+    <!-- /Yandex.Metrika counter -->
 </svelte:head>
 
 <noscript>
