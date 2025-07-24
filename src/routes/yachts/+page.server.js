2ef80c854e69175c252dfe4cce1861af398ffc55
mikle1978-mo <azimuth-m@yandex.ru>
2025-07-24 22:07:44 +0300

    250724 cache

--- src/routes/yachts/+page.server.js
+++ src/routes/yachts/+page.server.js
@@ -1,8 +1,8 @@
 import { composeCards } from "$lib/server/cards/composeCards";
 import { getCache, setCache } from "$lib/server/cache.js";
+import { CACHE_TTL_SECONDS } from "$lib/constants/cacheTtlSeconds";
 
 const CACHE_KEY = "yachts"; // Можно включить язык, тип и т.п.
-const CACHE_TTL_SECONDS = 24 * 60 * 60; // 1 день
 
 export async function load() {
     let yachts = await getCache(CACHE_KEY);
