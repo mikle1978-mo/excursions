79bbbdac5ccaeb8645864eb6065328da9acd9b0c
mikle1978-mo <azimuth-m@yandex.ru>
2025-11-04 12:26:32 +0300

    251104 google adsense

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -76,6 +76,12 @@
             <link rel="preload" as="image" href={item.img} type="image/webp" />
         {/if}
     {/each}
+    // Google AdSense
+    <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5449189100205390"
+        crossorigin="anonymous"
+    ></script>
 </svelte:head>
 
 <div class="content">
